da5e0d694e701844b59ce912001c934a2af4bd5d
Joe.cheng <joe.cheng@corp.to8to.com>
2024-05-15 20:24:19 +0800

    backup

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      auto-complete="on"
+      label-position="left"
+    >
       <div class="title-container">
-        <h3 class="title">黑马程序员Tlias智能学习辅助系统</h3>
+        <h3 class="title">辅助系统</h3>
       </div>
 
       <el-form-item prop="username">
@@ -37,104 +43,115 @@
           @keyup.enter.native="handleLogin"
         />
         <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+          <svg-icon
+            :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
+          />
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
-
+      <el-button
+        :loading="loading"
+        type="primary"
+        style="width: 100%; margin-bottom: 30px"
+        @click.native.prevent="handleLogin"
+        >Login</el-button
+      >
     </el-form>
   </div>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
-import { login } from '@/api/user'
-import { setToken } from '@/utils/auth'
+import { validUsername } from "@/utils/validate";
+import { login } from "@/api/user";
+import { setToken } from "@/utils/auth";
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
-    //用户名校验规则
+    // 用户名校验规则
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('请输入正确的用户名'))
+        callback(new Error("请输入正确的用户名"));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
 
-    //用户名校验规则
+    // 用户名校验规则
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('密码长度至少为6位'))
+        callback(new Error("密码长度至少为6位"));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
 
-    //数据模型
+    // 数据模型
     return {
       loginForm: {
-        username: 'jinyong',
-        password: '123456'
+        username: "jinyong",
+        password: "123456",
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        username: [
+          { required: true, trigger: "blur", validator: validateUsername },
+        ],
+        password: [
+          { required: true, trigger: "blur", validator: validatePassword },
+        ],
       },
       loading: false,
-      passwordType: 'password',
-      redirect: undefined
-    }
+      passwordType: "password",
+      redirect: undefined,
+    };
   },
 
   methods: {
-    //展示密码
+    // 展示密码
     showPwd() {
-      if (this.passwordType === 'password') {
-        this.passwordType = ''
+      if (this.passwordType === "password") {
+        this.passwordType = "";
       } else {
-        this.passwordType = 'password'
+        this.passwordType = "password";
       }
       this.$nextTick(() => {
-        this.$refs.password.focus()
-      })
+        this.$refs.password.focus();
+      });
     },
-    
+
     // 登录方法
     handleLogin() {
-      this.$refs.loginForm.validate(valid => {
+      this.$refs.loginForm.validate((valid) => {
         if (valid) {
-          this.loading = true
-          
-          //调用登录后端接口
+          this.loading = true;
+
+          // 调用登录后端接口
           login(this.loginForm).then((result) => {
-            console.log(result)
+            console.log(result);
             if (result.data.code == 1) {
               setToken(result.data.data);
-              console.log('login success');
-              this.$router.push('/');
+              console.log("login success");
+              this.$router.push("/");
             } else {
               this.$message.error(result.data.msg);
-              this.loading = false
+              this.loading = false;
             }
           });
         } else {
-          console.log('error submit!!')
-          return false
+          console.log("error submit!!");
+          return false;
         }
-      })
-    }
-  }
-}
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
-$light_gray:#fff;
+$bg: #283443;
+$light_gray: #fff;
 $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -177,9 +194,9 @@ $cursor: #fff;
 </style>
 
 <style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
 .login-container {
   min-height: 100%;
@@ -225,7 +242,7 @@ $light_gray:#eee;
       margin: 0px auto 40px auto;
       text-align: center;
       font-weight: bold;
-      font-family: '楷体';
+      font-family: "楷体";
     }
   }
 
